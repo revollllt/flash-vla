68acca66f6f359751b158c9bd813e70bbcba70cb
revollllt <1738157590@qq.com>
2026-09-15 02:43:11 +0800

    Pack Pi0.5 RTX 5090 expert gate and up into one GEMM

--- src/flash_vla/hardware/nvidia/rtx5090/pi05/backends/fused_ffn.py
+++ src/flash_vla/hardware/nvidia/rtx5090/pi05/backends/fused_ffn.py
@@ -42,6 +42,8 @@ def _library():
     lib.gated_activation_launch.argtypes = [ctypes.c_void_p] * 5 + [
         ctypes.c_int32, ctypes.c_void_p]
     lib.gated_activation_launch.restype = ctypes.c_int32
+    lib.packed_gated_activation_launch.argtypes = lib.gated_activation_launch.argtypes
+    lib.packed_gated_activation_launch.restype = ctypes.c_int32
     return lib
 
 
